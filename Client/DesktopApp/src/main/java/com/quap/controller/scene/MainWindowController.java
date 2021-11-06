10436de0af36560fecb457dda193068c563e0fa2
Lennart Protte <lenni.protte@gmx.de>
2021-11-06 21:56:10 +0100

    Liquid Design in MainWindowController.java and main-window.fxml

--- Client/DesktopApp/src/main/java/com/quap/controller/scene/MainWindowController.java
+++ Client/DesktopApp/src/main/java/com/quap/controller/scene/MainWindowController.java
@@ -3,13 +3,40 @@ package com.quap.controller.scene;
 import com.quap.controller.vista.VistaNavigator;
 import com.quap.controller.vista.login.LoginVistaNavigator;
 import com.quap.controller.vista.main.MainVistaNavigator;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class MainWindowController {
     private MainVistaNavigator currentNode;
     private final Vista vista = new Vista();
 
+    @FXML
+    private StackPane stackContent;
+
+    @FXML
+    private BorderPane paneSidebar;
+
+    @FXML
+    private VBox vBoxButtonHolder;
+
+    @FXML
+    private Label lblServer_IP;
+
+
     public void setVista(Object load) {
+    }
+
+    public void maximize(ActionEvent actionEvent) {
+    }
+
+    public void minimize(ActionEvent actionEvent) {
+    }
+
+    public void close(ActionEvent actionEvent) {
     }
 
     private class Vista extends LoginVistaNavigator {
